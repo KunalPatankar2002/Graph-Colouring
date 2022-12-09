150df706bdc01cbab4778900a1f6f76163abedd7
kunal <kunalpatankar.2002@gmail.com>
2022-12-09 21:12:23 +0530

    added comments

--- java/graphcolouring.java
+++ java/graphcolouring.java
@@ -22,31 +22,33 @@ public class graphcolouring {
     }
 
     static void colourGraph(ArrayList<ArrayList<Integer>> graph, int v) {
-        int[] vertexColour = new int[v];
-        ArrayList<Integer> colours = new ArrayList<Integer>();
-        Arrays.fill(vertexColour, -1);
+        int[] vertexColour = new int[v]; // array to store the colour of each vertex
+        Arrays.fill(vertexColour, -1); // initialise the array to -1
+        ArrayList<Integer> usedColours = new ArrayList<Integer>(); // stores the colours that have been used in previous
+        // vertices
+        ArrayList<Integer> availcolours = new ArrayList<Integer>(v); // stores the colours that are available to use
         int i = 0;
-
-        vertexColour[i++] = 0;
-        while (true) {
-            if (IntStream.of(vertexColour).anyMatch(x -> x == -1)) {
-                ArrayList<Integer> availcolours = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6));
-                for (int elem : graph.get(i)) {
-                    if (vertexColour[elem] != -1) {
-                        colours.add(vertexColour[elem]);
-                    }
+        vertexColour[i++] = 0; // assign the first vertex the first colour
+        while (IntStream.of(vertexColour).anyMatch(x -> x == -1)) { // run until all vertices have been assigned a
+                                                                    // colour
+            for (int j = 0; j < v; j++) // initialise available colours with all possible colours
+                availcolours.add(j);
+            for (int elem : graph.get(i)) { // add every colour used in adjacent vertices
+                if (vertexColour[elem] != -1) {
+                    usedColours.add(vertexColour[elem]);
                 }
-                availcolours.removeAll(colours);
-                Collections.sort(availcolours);
-                vertexColour[i] = availcolours.get(0);
-                colours.clear();
-                i++;
-            } else {
-                break;
             }
+            availcolours.removeAll(usedColours); // remove the used colours from the list of available colours
+            Collections.sort(availcolours); // sort remaining available colours
+            vertexColour[i] = availcolours.get(0); // assign the smallest available colour to the current vertex
+            usedColours.clear(); // clear the variables
+            availcolours.clear();
+            i++; // next vertex
         }
-        for (int a : vertexColour)
-            System.out.println(a);
+        i = 0;
+        for (int a : vertexColour) // prints the colour of each vertex
+            System.out.println("Colour of vertex " + i++ + " is " + a);
+        System.out.println("Chromatic number is " + (Arrays.stream(vertexColour).max().getAsInt() + 1));
     }
 
     public static void main(String[] args) {
@@ -57,23 +59,28 @@ public class graphcolouring {
         ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>(vertices);
         for (int i = 0; i < vertices; i++)
             graph.add(new ArrayList<Integer>());
-        while (ch != 0) {
-            System.out.println("Enter the two vertices of the edge");
-            int n = sc.nextInt(), m = sc.nextInt();
-            addEdge(graph, n, m);
-            System.out.println("Enter 0 to stop input of edges\n1 to continue");
-            ch = sc.nextInt();
-        }
+        // while (ch != 0) {
+        // System.out.println("Enter the two vertices of the edge");
+        // int n = sc.nextInt(), m = sc.nextInt();
+        // addEdge(graph, n, m);
+        // System.out.println("Enter 0 to stop input of edges\n1 to continue");
+        // ch = sc.nextInt();
+        // }
 
-        // addEdge(graph, 0, 1);
-        // addEdge(graph, 0, 4);
-        // addEdge(graph, 1, 2);
-
-        // addEdge(graph, 1, 4);
-        // addEdge(graph, 2, 3);
-        // addEdge(graph, 3, 4);
+        addEdge(graph, 0, 1);
+        addEdge(graph, 0, 3);
+        addEdge(graph, 0, 5);
+        addEdge(graph, 0, 6);
+        addEdge(graph, 1, 2);
+        addEdge(graph, 1, 4);
+        addEdge(graph, 2, 3);
+        addEdge(graph, 2, 5);
+        addEdge(graph, 2, 6);
+        addEdge(graph, 3, 4);
+        addEdge(graph, 4, 5);
+        addEdge(graph, 4, 6);
         colourGraph(graph, vertices);
-        printGraph(graph);
+        // printGraph(graph);
         sc.close();
     }
 }
